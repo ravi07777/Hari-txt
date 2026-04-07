9c52b343fb2d4b8bcc7f2e4b1dd9cf9d7e72ceb0
ravi07777 <ravindrax7878@gmail.com>
2026-04-07 10:04:18 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # loads from .env file locally; on Render uses environment variables
 
-API_ID   = int(os.environ.get("API_ID", ""))
+API_ID   = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
